5d6252aa47283b3b71303f178d2872e4b2be44f9
Kartik Dhomne <kartik.dhomne@acceleratebs.com>
2024-05-04 22:46:56 +0530

    Supabase connection added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i5.walmartimages.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bnvyjtexrucyruascdbp.supabase.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
